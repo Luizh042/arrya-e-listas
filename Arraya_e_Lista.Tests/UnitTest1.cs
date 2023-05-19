02230203e7339ae76757c32bc296a0d31b41f11c
Luizh042 <coisaqualquer996@gmail.com>
2023-05-19 20:05:46 -0300

    calculo de soma e media

--- Arraya_e_Lista.Tests/UnitTest1.cs
+++ Arraya_e_Lista.Tests/UnitTest1.cs
@@ -15,4 +15,52 @@ class Test
         int retornosoma=_calculadora.Soma(numeros);
         Assert.AreEqual(retornosoma,15);
     }
-}
+    [Test]
+    public void TestMedia()
+    {
+        List<int> numeros = new List<int> {1, 2, 3, 4, 5};
+        int quantidade=5;
+        double retornomedia=_calculadora.Media(numeros,quantidade);
+        Assert.AreEqual(retornomedia, 3);
+    }
+     [Test]
+    public void TestMedia6Numeros()
+    {
+        List<int> numeros = new List<int> {1, 2, 3, 4, 5, 6};
+        int quantidade=6;
+        double retornomedia=_calculadora.Media(numeros,quantidade);
+        Assert.AreEqual(retornomedia, 3.5);
+    }
+      [Test]
+    public void TestMedia12Numeros()
+    {
+        List<int> numeros = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int quantidade=12;
+        double  test = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12);
+        double testmedia = test/quantidade;
+        double retornomedia=_calculadora.Media(numeros,quantidade);
+        Assert.AreEqual(retornomedia, testmedia);
+    }
+     [Test]
+    public void TestMedia24Numeros()
+    {
+        List<int> numeros = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int quantidade=24;
+        double  test = _calculadora.Soma(numeros);
+        double testmedia = test/quantidade;
+        double retornomedia=_calculadora.Media(numeros,quantidade);
+        Assert.AreEqual(retornomedia, testmedia);
+    }
+     [Test]
+    public void TestMedia48Numeros()
+    {
+        List<int> numeros = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7,2,4,5,6,7,7,7,7,7,6,6,5,4,6,5,4,32,5,3,5,4,3,5, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int quantidade=100;
+        double  test = _calculadora.Soma(numeros);
+        double testmedia = test/quantidade;
+        double retornomedia=_calculadora.Media(numeros,quantidade);
+        Assert.AreEqual(retornomedia,testmedia, 0.01,"");
+        Console.WriteLine("1" + retornomedia);
+        Console.WriteLine("2" + testmedia);
+    }
+}   
--- Arraya_e_Lista/Calculadora.cs
+++ Arraya_e_Lista/Calculadora.cs
@@ -2,11 +2,14 @@
 {
     public class Calculadora
     {
+        
         public static void Main(string[] args)
         {
-
+             List<int> numeros = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+            Console.WriteLine("Soma: " + Soma(numeros));
+            Console.WriteLine("Média: " + Media(numeros));
         }
-        public int Soma(List<int> numeros)
+        public static int Soma(List<int> numeros)
         {
              int soma = 0;
             foreach (int num in numeros)
@@ -15,6 +18,13 @@
             }
             return soma;
 
+        }
+        public static double Media(List<int> numeros)
+        {
+            int soma = Soma(numeros);
+            double media = (double)soma / numeros.Count;
+            return media;
+          
         }
     }
 }
